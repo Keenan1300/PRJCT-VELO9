fdec60a67b93588d574b02e1e6945b9f6168d98b
DESKTOP-VTO8RSG\DANNY <hamilt25@sheridancollege.ca>
2025-02-23 19:17:32 -0500

    Addbasic character movement

--- Experimentation/Assets/Scripts/FirstScript.cs
+++ Experimentation/Assets/Scripts/FirstScript.cs
@@ -1,23 +1,45 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
 public class FirstScript : MonoBehaviour
 {
+    float Speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Speed = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.Getkey(KeyCode.A))
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.A))
         {
-            
+            pos.x -= Speed * Time.deltaTime; 
         }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += Speed * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.z += Speed * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.z -= Speed * Time.deltaTime; 
+        }
+
+        transform.position = pos;
+        
+
     }
 }
