03b7edee1526168df8054232460363c78db0a885
Keenan1300 <hamilt25@sheridancollege.ca>
2025-07-27 12:15:47 -0400

    SetBoolsFor Animations

--- Experimentation/Assets/Scripts/HarbAnimations.cs
+++ Experimentation/Assets/Scripts/HarbAnimations.cs
@@ -16,7 +16,14 @@ public class HarbAnimations : MonoBehaviour
 
     void Start()
     {
-      
+        anim = GetComponent<Animator>();
+        anim.SetBool("Idle", true);
+        anim.SetBool("RightStrafe", false);
+        anim.SetBool("LeftStrafe", false);
+        anim.SetBool("BackStrafe", false);
+        anim.SetBool("Run", false);
+        anim.SetBool("Grab", false);
+        anim.SetBool("GunFire", false);
     }
 
     void Update()
