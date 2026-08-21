b347d79e122f1969f03fc06bd76136bdbc0846a0
Keenan1300 <hamilt25@sheridancollege.ca>
2026-08-21 15:03:17 -0400

    grid

---
+++ Experimentation/Assets/Scripts/GridGen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GridGen : MonoBehaviour
+{
+
+    public  int width;
+    public int hieght;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
