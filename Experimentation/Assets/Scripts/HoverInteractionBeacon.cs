40271db1e8ab020a22fe3faf32e19456f5114187
Keenan1300 <hamilt25@sheridancollege.ca>
2026-08-24 15:12:11 -0400

    added basic SDFX

--- Experimentation/Assets/Scripts/HoverInteractionBeacon.cs
+++ Experimentation/Assets/Scripts/HoverInteractionBeacon.cs
@@ -7,23 +7,33 @@ public class HoverInteractionBeacon : MonoBehaviour
     public Sprite HoveredIcon;
     public Image ImageComp;
     public float scaleupfactor;
+    private AudioSource AudioSource;
+    public AudioClip BeaconHover;
     //public float defaultscale;
 
     public void Start()
     {
         ImageComp = GetComponent<Image>();
+        AudioSource = GetComponent<AudioSource>();  
     }
 
     public void HoverInteractionEnter() 
     {
         ImageComp.sprite = HoveredIcon;
         transform.localScale *= scaleupfactor;
+        AudioSource.Play();
     }
 
     public void HoverInteractionExit()
     {
         ImageComp.sprite = UnhoveredIcon;
         transform.localScale /= scaleupfactor;
+    }
+
+    public void BeaconSelected()
+    {
+        //Target locked
+
     }
 
 }
