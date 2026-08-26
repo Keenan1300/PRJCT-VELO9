3b355778e49463b3c91f679dd8bb1bc718f801c8
Keenan1300 <hamilt25@sheridancollege.ca>
2026-08-26 18:54:43 -0400

    Pushing CSV data

---
+++ Experimentation/Assets/Editor/CSVparser.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEditor.Timeline.Actions;
+
+public class CSVparser
+{
+
+
+    private static string CargoDataPath = "/Editor/CSVs/CargoDataValues.csv";
+
+    [MenuItem("Utilities/GenerateCargo")]
+
+    public static void GenerateCargo()
+    {
+    
+        string[] alllines = File.ReadAllLines(Application.dataPath + CargoDataPath);
+        Debug.Log("Generating Cargo...");
+        foreach (string s in alllines)
+        {
+            string[] Splitdata = s.Split(',');
+
+
+            //Safety net
+            if (Splitdata.Length != 9) 
+            {
+                Debug.Log(s +" has incorrect data values");
+                return;
+            }
+
+            CargoData Cargo = ScriptableObject.CreateInstance<CargoData>();
+
+
+            Cargo.CargoID = int.Parse(Splitdata[0]);
+            Cargo.CargoName = Splitdata[1];
+            Cargo.Description = Splitdata[2];
+            Cargo.SPeffects = Splitdata[3];
+            Cargo.O2Refill = float.Parse(Splitdata[4]);
+            Cargo.FuelRefill = float.Parse(Splitdata[5]);
+            Cargo.MoraleRefill = float.Parse(Splitdata[6]);
+            Cargo.Value = float.Parse(Splitdata[7]);
+            Cargo.Cost = float.Parse(Splitdata[8]);
+
+            AssetDatabase.CreateAsset(Cargo, $"Assets/Cargo/{Cargo.CargoName}.asset");
+
+        }
+
+        AssetDatabase.SaveAssets();
+    }
+
+}
